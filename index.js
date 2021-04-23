783619ca039e79e28d72303e7dbd9e2c56e07f19
Lutimi <misael@wemake.pe>
2021-04-23 09:50:07 -0500

    Desafio 1

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// /// Ejercicio 1 
+const welcome = "Hola";
+
+let nombre = prompt("Ingresa tu nombre:");
+let apellido = prompt( "Ingresa tu apellido");
+
+alert(welcome + " " + nombre+" "+apellido );
+
+
+
+//Pide datos
+let n1 = prompt('Ingresa el primer numero');
+let n2 = prompt('Ingresa el segundo numero');
+
+
+// Conversion a int 
+let n1Int = parseInt(n1);
+let n2Int = parseInt(n2);
+
+
+
+const resultado = n1Int + n2Int;
+const resta = n1Int - n2Int;
+const multi = n1Int * n2Int;
+
+console.log("La suma es es:" +resultado);
+console.log("La resta es:" +resta);
+console.log("La multipicacion es :" +multi);
+alert("El resultado es:" +resultado)
+
