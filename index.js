3e985cb3dc19e9bb2ddc57fba97c686bc6e12a1c
Lutimi <misael@wemake.pe>
2021-04-28 21:25:07 -0500

    Desafio3

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
 
 
-
-// cosigna 1
-let numero = prompt("Ingrese un numero:");
-
-if ( numero > 1000){
-    alert("Este numero es mayor");
-}
-else{
-    console.log("No vasa ver este mensaje");
-}
+function solicitarNombre(){
+    let nombreIngresado   = prompt("Ingresar nombre");
+    console.log("El nombre ingresado es " + nombreIngresado);
+} 
+function solicitarEdad(){
+    let edadIngresada  = parseInt(prompt("Ingresar Edad"));
+    console.log("La edad ingresada es: " + edadIngresada);
+} 
 
 
+function convertir(dolar){
 
-Hola = "Hola"
-let palabra = prompt(" Ingrese un saludo:")
+    dolar = parseFloat(dolar);
+     
+    dolar = dolar * 3.80
 
-if( palabra == Hola){
-
-    alert(Hola+" "+"Como estas?" );
-}
-else{
-    console.log("No se va a mostrar nada");
-}
- let numero2 = prompt("ingrese un numero:")
-
-if (  numero > 10  && numero < 50) {
-
-    alert("Este numero se encuentra en el rango");
+    console.log(dolar);
 
 }
-else{
-    console.log("No se encuentra en el rango")
-}
+
+var menu =true;
+console.log("Bienvenido a TuVencidario \nApp desarrollada para la seguridad de tu condominio\n1 Registro\n2 Promedio\n3 Total de personas\n0 Salir")
+
+while( menu ){
+
+    var promedio_edades = 0;
+    var  nroPersonas = 0;
+
+    console.log
+
+    var operacion = prompt("Elija una opcion");
+
+    if( operacion == "0"){
+
+        menu = false;
+    }
+
+    switch (operacion){
+    case '1':
+        var personas = parseInt(prompt("Ingrese el numero de personas a registrar"));
+        
+    for( i = 0 ; i < personas; i++){
+        solicitarNombre();
+        solicitarEdad();
+        numerox = (i+1);
+        nroPersonas++;
+
+    } 
+        
+        console.log( "["+ numerox +"]" + "La cantidad de personas registradas hasta el momento es:"+ nroPersonas )
+        break;
+    
+    case '2':
+    
+    var cambio = parseFloat(prompt("Ingrese cantidad de Monedas en sol peruano que desea a cambiar a dolares: "));
+
+    if ( cambio > 50){
+
+        convertir(cambio);
+        
+    }
+    else {
+        console.log("solo cambios se aceptan cambios mayores a 100 soles");
+    }
+  
+        break;
+    
+    case 'default':
+
+    console.log("No se encuentra en el listado");
+    
 
 
 
+    }
+    // end of while
+    }
 
 
 
