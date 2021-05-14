f91eebfcc1a4ab64efcef10c7a5537d115c74ed4
Lutimi <misael@wemake.pe>
2021-05-14 17:32:08 -0500

    idk

--- index.js
+++ index.js
@@ -1,81 +1,56 @@
+// var data = ['Nombre','Edad','Fecha de Nacimiento']  
+// var pila = [];  
+    
+// function getInfo(info){
+//     return prompt(info);
+// }
 
-function solicitarNombre(){
-    let nombreIngresado   = prompt("Ingresar nombre");
-    console.log("El nombre ingresado es " + nombreIngresado);
-} 
-function solicitarEdad(){
-    let edadIngresada  = parseInt(prompt("Ingresar Edad"));
-    console.log("La edad ingresada es: " + edadIngresada);
-} 
+// function addToArray(item){
+//     return pila.push(item);
+// }
+
+// data.forEach(ele => addToArray(getInfo(ele)));
+
+// console.log(pila);
 
 
-function convertir(dolar){
+// function obtenerData(){
+// var nombre = prompt ('Ingresa nombre:');
+// var edad =  parseInt(prompt('Ingresa edad:'));
+// var fecha = parseInt(prompt('Ingresa año:'))
 
-    dolar = parseFloat(dolar);
-     
-    dolar = dolar * 3.80
+// var pila = [nombre,edad,fecha]
 
-    console.log(dolar);
+// console.log('nombre '+ pila[0]+' '+ pila[1]+' Edad'+pila[2]+' año de nacimiento:');
+
+// }
+
+// obtenerData();
+
+
+// // const multi = (n1,n2) =>  parseFloat (n1 * n2);
+
+
+// // console.log(multi(20,5)) 
+
+
+// var persona = { 
+//     nombre = 'Luis',
+//     apellido = 'Ticona',
+//     edad = 21,
+//     nacionalidad = 'Peru',
+//     ocupacion = 'Estudiante'
+
+// }
+
+
+var perro = {
+
+    nombre = 'Gigi',
+    edad : 9,
+    habilidad: 'Jugar',
+    peso: 4,
+    ladrarAlgo: function(){ console.log('Guaf'+ this.nombre)},
+    edadAñosPerro: function() { console.log(this.edad * 7)}
 
 }
-
-var menu =true;
-console.log("Bienvenido a TuVencidario \nApp desarrollada para la seguridad de tu condominio\n1 Registro\n2 Convertor divisas \n0 Salir")
-
-while( menu ){
-
-    var promedio_edades = 0;
-    var  nroPersonas = 0;
-
-    console.log
-
-    var operacion = prompt("Elija una opcion");
-
-    if( operacion == "0"){
-
-        menu = false;
-    }
-
-    switch (operacion){
-    case '1':
-        var personas = parseInt(prompt("Ingrese el numero de personas a registrar"));
-        
-    for( i = 0 ; i < personas; i++){
-        solicitarNombre();
-        solicitarEdad();
-        numerox = (i+1);
-        nroPersonas++;
-
-    } 
-        
-        console.log( "["+ numerox +"]" + "La cantidad de personas registradas hasta el momento es:"+ nroPersonas )
-        break;
-    
-    case '2':
-    
-    var cambio = parseFloat(prompt("Ingrese cantidad de Monedas en sol peruano que desea a cambiar a dolares: "));
-
-    if ( cambio > 50){
-
-        convertir(cambio);
-        
-    }
-    else {
-        console.log("solo cambios se aceptan cambios mayores a 100 soles");
-    }
-  
-        break;
-    
-    case 'default':
-
-    console.log("No se encuentra en el listado");
-    
-
-
-
-    }
-    // end of while
-    }
-
-
-
